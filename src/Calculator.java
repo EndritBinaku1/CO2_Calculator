5cacbe0e02f79028ed815962b56443b3e12476bf
EndritBinaku <s51060@edu.campus02.at>
2021-10-08 19:59:35 +0200

    Variable Declaration

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,20 @@
 public class Calculator {
     public static void main(String[] args) {
 
+        int a = 1;
+        System.out.println(a);
+
+        double b = 2.0;
+        System.out.println(b);
+
+        char c = 'c';
+        System.out.println(c);
+
+        boolean d = true;
+        System.out.println(d);
+
+        String g = "Hello World!";
+        System.out.println(g);
+
     }
 }
