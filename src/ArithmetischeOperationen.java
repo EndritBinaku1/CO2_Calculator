486c6e4afdd21de245ec17a5d1431d0c85c23e33
EndritBinaku <s51060@edu.campus02.at>
2021-10-08 20:23:22 +0200

    Arithmetiesche Operationen

---
+++ src/ArithmetischeOperationen.java
@@ -0,0 +1,23 @@
+public class ArithmetischeOperationen {
+    public static void main(String[] args) {
+
+        int c, e, g, h, l;
+        int a = 5;
+        int b = 10;
+
+        c = a + b;
+        e = b - a;
+        g = a * b;
+        h = b / a;
+        l = a % b;
+
+        System.out.println(c);
+        System.out.println(e);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(l);
+
+
+
+    }
+}
