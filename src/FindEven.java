3d379b458e075a6ab707c43c2fdacabef2695844
EndritBinaku <s51060@edu.campus02.at>
2021-10-09 10:18:10 +0200

    While Loop

---
+++ src/FindEven.java
@@ -0,0 +1,13 @@
+public class FindEven {
+    public static void main(String[] args) {
+
+        int counter = 2;
+        int input = 10;
+
+        while (counter <= input){
+
+            System.out.println("Input: " + counter);
+            counter += 2;
+        }
+    }
+}
